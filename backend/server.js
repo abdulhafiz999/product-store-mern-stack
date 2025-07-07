b7b418f1394f828effad718420ba8b24892b2149
Hafiz tech <abdulhafiz99888@gmail.com>
2025-07-07 23:59:25 +0100

    Add .env configuration and MongoDB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,32 @@
 //import the dotenv to load environment variables 
 import dotenv from 'dotenv';
+dotenv.config();
 
 //import express
 import express from 'express';
+import { dot } from 'node:test/reporters';
 
 //get the express app instance
 const app = express();
 
+//create a get request handler for the root path
+app.get('/', (req, res) => {
+    //send a response
+    res.send('Hello World from Express');
+});
+
+
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+    { id: 3, name: 'Alice Johnson' }
+  ]);
+})
+const PORT = process.env.PORT;
+
+app.
 //start the server
-app.listen(5000, () => {
-    console.log('Server is running on port 5000 and we are ready to go!');
+app.listen(PORT, () => {
+    console.log(`Server is running on port 5000 and we are ready to go! ${PORT}`);
 });
